2de53da992153e62b09ea3f7284d9f794816af46
Mehmet ALPEREN <me.alperen@gmail.com>
2022-02-20 14:15:24 +0300

    Constants and CountryCards fixed.

--- countries-app/src/components/country-card/CountryCard.js
+++ countries-app/src/components/country-card/CountryCard.js
@@ -1,10 +1,10 @@
 import './style.css';
 
-export function CountryCard({ name, population, region, capital, flag }) {
+export function CountryCard({ name, population, region, capital, flags }) {
   return (
     <div className='country-card'>
-      <img src={flag} alt={flag} />
-      <h3 className='country-name'>{name}</h3>
+      <img src={flags.png} />
+      <h3 className='country-name'>{name.official}</h3>
       <h4 className='country-population'>{population}</h4>
       <h4 className='country-region'>{region}</h4>
       <h4 className='country-capital'>{capital}</h4>
--- countries-app/src/constants/api.js
+++ countries-app/src/constants/api.js
@@ -2,4 +2,4 @@ export const BASE_URL = 'https://restcountries.com/v3.1/';
 
 export const URL_ALL_COUNTRIES = `${BASE_URL}all`;
 
-export const URL_COUNTRY = `${BASE_URL}name`;
+export const URL_COUNTRY = `${BASE_URL}name/`;
