18e060183f904400888d5833744313fa904bcea5
Mehmet ALPEREN <me.alperen@gmail.com>
2022-03-18 23:34:44 +0300

    Single country refactored.

--- countries-app/src/pages/single-country-page/SingleCountry.js
+++ countries-app/src/pages/single-country-page/SingleCountry.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { URL_COUNTRY } from '../../constants/api';
+import { Box, Flex, Image, Text } from '@chakra-ui/react';
 
 export function SingleCountry() {
   const [country, setCountry] = useState({});
@@ -22,43 +23,49 @@ export function SingleCountry() {
   console.log(country[0]);
 
   return (
-    <div>
-      <div className='section-center wrapper'>
-        <div>
-          <img
-            src={country[0]?.flags.png}
-            alt='country'
-            className='box single-image'
-          />
-        </div>
-        <div className='box '>
-          <p className='left-side'>Population : {country[0]?.population}</p>
-          <p className='left-side'>
-            Native Name: {country[0]?.name?.nativeName?.spa?.official}
-          </p>
-          <p className='left-side'>Region : {country[0]?.region}</p>
-          <p className='left-side'>
-            Currencies :
-            {
-              country[0]?.currencies[Object.keys(country[0]?.currencies)[0]]
-                .name
-            }
-          </p>
-          <p className='right-side'>Subregion : {country[0]?.subregion}</p>
-          <p className='right-side'>Capital : {country[0]?.capital}</p>
-          <p className='right-side'>Top Level Domain : {country[0]?.tld}</p>
-          <p className='right-side'>
-            Languages :
-            {country[0]?.languages[Object.keys(country[0]?.languages)[0]]}
-          </p>
-          <div>
-            Borders:
-            {country[0]?.borders?.map((border, index) => {
-              return <p key={index}>{border}</p>;
-            })}
-          </div>
-        </div>
-      </div>
-    </div>
+    <Flex className='section-center wrapper'>
+      <Box>
+        <Image
+          src={country[0]?.flags.png}
+          alt='country'
+          className='box single-image'
+          width='50rem'
+        />
+      </Box>
+      <Box className='box'>
+        <Text fontSize='xl' fontWeight='bold'>
+          <Text fontSize='lg'>Population : {country[0]?.population}</Text>
+        </Text>
+        <Text fontSize='xl' fontWeight='bold'>
+          Native Name: {country[0]?.name?.nativeName?.spa?.official}
+        </Text>
+        <Text fontSize='xl' fontWeight='bold'>
+          Region : {country[0]?.region}
+        </Text>
+        <Text fontSize='xl' fontWeight='bold'>
+          Currencies :
+          {country[0]?.currencies[Object.keys(country[0]?.currencies)[0]].name}
+        </Text>
+        <Text fontSize='xl' fontWeight='bold'>
+          Subregion : {country[0]?.subregion}
+        </Text>
+        <Text fontSize='xl' fontWeight='bold'>
+          Capital : {country[0]?.capital}
+        </Text>
+        <Text fontSize='xl' fontWeight='bold'>
+          Top Level Domain : {country[0]?.tld}
+        </Text>
+        <Text fontSize='xl' fontWeight='bold'>
+          Languages :
+          {country[0]?.languages[Object.keys(country[0]?.languages)[0]]}
+        </Text>
+        <Text fontSize='xl' fontWeight='bold'>
+          Borders:
+          {country[0]?.borders?.map((border, index) => {
+            return <Text key={index}>{border}</Text>;
+          })}
+        </Text>
+      </Box>
+    </Flex>
   );
 }
